ab70042d557624f4d14cccc65bb0881492bf40b3
Beshu Gowdaman <beshu4959gowdaman@gmail.com>
2025-12-23 18:00:29 +0530

    handle invalid short code scenarios

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -214,3 +214,4 @@ exports.updateUrl = async (req, res, next) => {
 // handle create short url request
 // generate unique short code
 // handle redirect using short code
+// handle invalid short code
